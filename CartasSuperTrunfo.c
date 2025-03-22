bcba6ffa3c5eee36f9a42f600502a0bb44146c7d
Harley-7 <harleyoliveira008@gmail.com>
2025-03-22 23:11:56 +0000

    desafio aventureiro concluído

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,10 @@ int main() {
     char nome_da_cidade_carta1[50];
     int populacao_carta1, 
     numero_de_pontos_turisticos_carta1;
-
     float area_carta1,
-    pib_carta1;
+    pib_carta1,
+    densidade_populacional_carta1,
+    pib_per_capita_carta1;
 
     //variaveis da carta 2
     char estado_carta2;
@@ -16,7 +17,10 @@ int main() {
     int populacao_carta2, 
     numero_de_pontos_turisticos_carta2;
     float area_carta2,
-    pib_carta2;
+    pib_carta2,
+    densidade_populacional_carta2,
+    pib_per_capita_carta2;
+    
 
     //Entrada do dados digitados pelo usuário no terminal
 
@@ -66,9 +70,11 @@ int main() {
     printf("Código: %c01 \n", estado_carta1);
     printf("Nome da Cidade: %s \n", nome_da_cidade_carta1);
     printf("População: %d \n", populacao_carta1);
-    printf("Área: %f \n", area_carta1);
+    printf("Área: %f km²\n", area_carta1);
     printf("PIB: %f \n", pib_carta1);
     printf("Número de pontos turísticos: %d \n", numero_de_pontos_turisticos_carta1);
+    printf("Densidade Populacional: %.2f hab/km² \n", densidade_populacional_carta1 = populacao_carta1 / area_carta1); //saída e calculo da densidade populacional da carta 1
+    printf("PIB per Capita: %.2f reais \n", pib_per_capita_carta1 = pib_carta1 / populacao_carta1); // saída e calculo do PIB per Capita da carta 1
     printf("\n");
 
     //Dados da Carta 2
@@ -77,9 +83,11 @@ int main() {
     printf("Código: %c02 \n", estado_carta2);
     printf("Nome da Cidade: %s \n", nome_da_cidade_carta2);
     printf("População: %d \n", populacao_carta2);
-    printf("Área: %f \n", area_carta2);
+    printf("Área: %f km² \n", area_carta2);
     printf("PIB: %f \n", pib_carta2);
     printf("Número de pontos turísticos: %d \n", numero_de_pontos_turisticos_carta2);
+    printf("Densidade Populacional: %.2f km² \n", densidade_populacional_carta2 = populacao_carta2 / area_carta2); //saída e calculo da densidade populacional da carta 2
+    printf("PIB per Capita: %.2f reais \n", pib_per_capita_carta2 = pib_carta2 / populacao_carta2); // saída e calculo do PIB per Capita da carta 2
     printf("\n");
 
     return 0;
